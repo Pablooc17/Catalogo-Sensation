0ee4ed12f3273f71ad4eee58d400e390d3172731
Pablooc17 <j-p-o9710@hotmail.es>
2026-05-11 18:40:26 -0500

    carga1

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+
+export default function App() {
+  return (
+    <div style={{fontFamily:'Arial',padding:'40px'}}>
+      <h1>SENSASION</h1>
+      <p>Proyecto funcionando correctamente en Netlify.</p>
+    </div>
+  )
+}
